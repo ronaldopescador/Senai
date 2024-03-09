cc04e28f299c5b2497e21d58464b5a314c1e0d66
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-09 10:33:09 -0300

    Reverso

---
+++ AlgoritmosCSharp/Turma91625/Introducao/Exercicio04_Cambio/Exercicio04_Cambio/Program.cs
@@ -0,0 +1,23 @@
+﻿// Conversão de moedas 
+
+// Obtendo o valor a ser convertido do usuário
+Console.WriteLine("Digite o valor a ser convertido:");
+double valorConverter = Convert.ToDouble(Console.ReadLine());
+
+// Obtendo a moeda de origem do usuário
+Console.WriteLine("Digite a moeda de origem:");
+string moedaOrigem = Console.ReadLine();
+
+// Obtendo a moeda de destino do usuário
+Console.WriteLine("Digite a moeda de destino:");
+string moedaDestino = Console.ReadLine();
+
+// Obtendo a taxa de câmbio do usuário
+Console.WriteLine("Digite a taxa de câmbio:");
+double taxaCambio = Convert.ToDouble(Console.ReadLine());
+
+// Calculando o valor convertido
+double valorConvertido = valorConverter * taxaCambio;
+
+// Exibindo o valor convertido
+Console.WriteLine("O valor convertido em {0} é: {1:F2}", moedaDestino, valorConvertido);
---
+++ AlgoritmosCSharp/Turma91625/Introducao/Exercicio05_Reverso/Exercicio05_Reverso/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Digite um número de 4 dígitos: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+int unidade = numero % 10; // Obtém o último dígito
+int dezena = (numero / 10) % 10; // Obtém o terceiro dígito
+int centena = (numero / 100) % 10; // Obtém o segundo dígito
+int milhar = (numero / 1000) % 10; // Obtém o primeiro dígito
+
+Console.WriteLine($"Número de trás para frente: {unidade}{dezena}{centena}{milhar}");
