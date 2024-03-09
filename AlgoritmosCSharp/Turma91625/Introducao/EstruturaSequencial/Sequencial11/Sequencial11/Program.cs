22ec08f743711d896a11d508ab087fd3fca96948
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-09 17:00:50 -0300

    Sequencial11

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial11/Sequencial11/Program.cs
@@ -0,0 +1,21 @@
+﻿// Ler a quantidade de dias
+Console.Write("Digite a quantidade de dias: ");
+int diasTotais = Convert.ToInt32(Console.ReadLine());
+
+// Calcular anos
+int anos = diasTotais / 360;
+
+// Calcular dias restantes após os anos
+int diasRestantes = diasTotais % 360;
+
+// Calcular meses
+int meses = diasRestantes / 30;
+
+// Calcular dias restantes após os meses
+int dias = diasRestantes % 30;
+
+// Exibir o resultado
+Console.WriteLine($"{diasTotais} dias equivalem a:");
+Console.WriteLine($"{anos} ano(s)");
+Console.WriteLine($"{meses} mês(es)");
+Console.WriteLine($"{dias} dia(s)");
