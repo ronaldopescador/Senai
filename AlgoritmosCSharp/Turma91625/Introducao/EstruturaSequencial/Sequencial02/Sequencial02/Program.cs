ea9e210b3c056618453a5550ab8353a80500db4c
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-09 17:07:05 -0300

    Sequencial02

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial02/Sequencial02/Program.cs
@@ -0,0 +1,9 @@
+﻿// Ler a quantidade de cavalos comprados
+Console.Write("Digite a quantidade de cavalos comprados: ");
+int quantidadeCavalos = Convert.ToInt32(Console.ReadLine());
+
+// Calcular a quantidade de ferraduras
+int quantidadeFerraduras = quantidadeCavalos * 4;
+
+// Exibir a quantidade de ferraduras
+Console.WriteLine($"São necessárias {quantidadeFerraduras} ferraduras para equipar todos os cavalos.");
