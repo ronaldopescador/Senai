9bf3ac4bcb0e9eb4fbf73444c9dbcef201bd93a4
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-09 00:21:38 -0300

    Viagem

---
+++ AlgoritmosCSharp/Turma91625/Viagem/Viagem/Program.cs
@@ -0,0 +1,6 @@
+﻿double tempoViagem = 6;
+double velocidadeMedia = 80;
+double distancia = tempoViagem * velocidadeMedia;
+double litrosConsumidos = distancia / 12;
+Console.WriteLine("Distância percorrida: {0:F2} km", distancia);
+Console.WriteLine("Litros de combustível consumidos: {0:F2} litros", litrosConsumidos);
