717fb26bccad70f0b5da87afdca737d90f7276d4
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-09 17:14:02 -0300

    Sequencial03

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial03/Sequencial03/Program.cs
@@ -0,0 +1,25 @@
+﻿// Leitura da quantidade de pães franceses
+Console.Write("Digite a quantidade de pães franceses vendidos: ");
+int quantidadePao = Convert.ToInt32(Console.ReadLine());
+
+// Leitura da quantidade de broas vendidas
+Console.Write("Digite a quantidade de broas vendidas: ");
+int quantidadeBroa = Convert.ToInt32(Console.ReadLine());
+
+// Cálculo do valor total das vendas de pães
+double valorPao = quantidadePao * 0.60;
+
+// Cálculo do valor total das vendas de broas
+double valorBroa = quantidadeBroa * 6.50;
+
+// Cálculo do valor total das vendas
+double valorTotalVendas = valorPao + valorBroa;
+
+// Cálculo do valor a ser guardado na poupança (10% do total)
+double valorPoupanca = valorTotalVendas * 0.10;
+
+// Exibição do valor total das vendas
+Console.WriteLine($"Valor total das vendas: R${valorTotalVendas:F2}");
+
+// Exibição do valor a ser guardado na poupança
+Console.WriteLine($"Valor a ser guardado na poupança: R${valorPoupanca:F2}");
