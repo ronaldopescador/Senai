738ccc1a6d26a8777a97e5cdfb28a9ca4d963e35
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-09 17:30:57 -0300

    Sequencial06

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial06/Sequencial06/Program.cs
@@ -0,0 +1,9 @@
+﻿// Leitura do peso do prato montado 
+Console.Write("Digite o peso do prato montado (sem o peso do prato): ");
+double pesoPrato = Convert.ToDouble(Console.ReadLine());
+
+// Cálculo do valor total a pagar
+double totalPagar = pesoPrato * 12.00;
+
+// Exibição do valor total a pagar
+Console.WriteLine($"O valor a pagar é de {totalPagar:F2}.");
