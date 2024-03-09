ceb8cc44f97360656ed40e1509f5fe343edb6458
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-09 17:20:14 -0300

    Sequencial04

--- AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial04/Sequencial04/Program.cs
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial04/Sequencial04/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Leitura do nome da pessoa
+Console.Write("Digite o nome da pessoa: ");
+string nome = Console.ReadLine();
+
+// Leitura da idade da pessoa
+Console.Write("Digite a idade da pessoa: ");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+// Cálculo dos dias de vida
+int diasVida = idade * 365;
+
+// Exibição do nome, idade e dias de vida da pessoa
+Console.WriteLine($"{nome} tem {idade} anos e {diasVida} dias de vida.");
