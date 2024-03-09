5a4bfc2e7355b7adab34340bd5e1fbce63c8f6a2
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-09 10:16:18 -0300

    Idade

--- AlgoritmosCSharp/Turma91625/Introducao/Exercicio02_Media/Media/Program.cs
+++ AlgoritmosCSharp/Turma91625/Introducao/Exercicio02_Media/Media/Program.cs
@@ -1,6 +1,15 @@
-﻿Console.WriteLine("Digite a primeira nota:");
+﻿// Cálculo de média 
+
+// Obtendo a primeira nota do usuário
+Console.WriteLine("Digite a primeira nota:");
 double nota1 = Convert.ToDouble(Console.ReadLine());
+
+// Obtendo a segunda nota do usuário
 Console.WriteLine("Digite a segunda nota:");
 double nota2 = Convert.ToDouble(Console.ReadLine());
+
+// Calculando a média
 double media = (nota1 + nota2) / 2;
+
+// Exibindo a média
 Console.WriteLine("A média é: {0:F2}", media);
---
+++ AlgoritmosCSharp/Turma91625/Introducao/Exercicio03_Triangulo/Exercicio03_Triangulo/Program.cs
@@ -0,0 +1,15 @@
+﻿// Cálculo da área de um triângulo
+
+// Obtendo a base do triângulo do usuário
+Console.WriteLine("Digite a base do triângulo:");
+double baseTriangulo = Convert.ToDouble(Console.ReadLine());
+
+// Obtendo a altura do triângulo do usuário
+Console.WriteLine("Digite a altura do triângulo:");
+double alturaTriangulo = Convert.ToDouble(Console.ReadLine());
+
+// Calculando a área do triângulo
+double areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+
+// Exibindo a área do triângulo
+Console.WriteLine("A área do triângulo é: {0:F2}", areaTriangulo);
