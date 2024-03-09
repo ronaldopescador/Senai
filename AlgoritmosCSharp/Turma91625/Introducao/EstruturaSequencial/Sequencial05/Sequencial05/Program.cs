f187f6e36e206f2eada765d162601a3865ef0cbd
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-09 17:27:51 -0300

    Sequencial05

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial05/Sequencial05/Program.cs
@@ -0,0 +1,13 @@
+﻿// Leitura do valor do total do abastecimento
+Console.Write("Digite o valor total do abastecimento: ");
+double valorPagamento = Convert.ToDouble(Console.ReadLine());
+
+// Leitura do preço do litro da gasolina
+Console.Write("Digite o preço do litro da gasolina: ");
+double precoLitro = Convert.ToDouble(Console.ReadLine());
+
+// Cálculo da quantidade de litros
+double quantidadeLitros = valorPagamento / precoLitro;
+
+// Exibição da quantidade de litros
+Console.WriteLine($"O motorista conseguiu colocar {quantidadeLitros:F2} litros de gasolina no tanque.");
