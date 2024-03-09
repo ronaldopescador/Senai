6a11f14add5cb40629d1da3de41abfa67db6d656
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-09 17:41:14 -0300

    Sequencial07

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial07/Sequencial07/Program.cs
@@ -0,0 +1,13 @@
+﻿// Leitura do dia
+Console.Write("Digite o dia: ");
+int dia = Convert.ToInt32(Console.ReadLine());
+
+// Leitura do mês
+Console.Write("Digite o mês: ");
+int mes = Convert.ToInt32(Console.ReadLine());
+
+// Cálculo do número de dias passados
+int diasPassados = (mes - 1) * 30 + dia;
+
+// Exibição do número de dias passados
+Console.WriteLine($"Se passaram {diasPassados} dias desde o início do ano.");
