1c30046991b5c22025723f63c4cfcbfd9eed690c
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-09 17:04:27 -0300

    Sequencial01

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial01/Sequencial01/Program.cs
@@ -0,0 +1,13 @@
+﻿// Ler a largura do terreno     
+Console.Write("Digite a largura do terreno: ");
+double largura = Convert.ToDouble(Console.ReadLine());
+
+// Ler o comprimento do terreno do usuário
+Console.Write("Digite o comprimento do terreno: ");
+double comprimento = Convert.ToDouble(Console.ReadLine());
+
+// Calcular a área do terreno
+double areaTerreno = largura * comprimento;
+
+// Exibir a área do terreno
+Console.WriteLine($"A área do terreno é de {areaTerreno:F2} m².");
