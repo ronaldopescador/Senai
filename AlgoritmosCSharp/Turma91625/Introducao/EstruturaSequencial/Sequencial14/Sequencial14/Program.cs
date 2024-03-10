aa9b03334ad5e9b96a6781690d98154055c6b1f2
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-10 19:02:04 -0300

    Sequencial14

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial14/Sequencial14/Program.cs
@@ -0,0 +1,9 @@
+﻿// Leitura do raio da pizza
+Console.Write("Digite o raio da pizza: ");
+double raio = Convert.ToDouble(Console.ReadLine());
+
+// Cálculo da área da pizza
+double area = 3.14 * Math.Pow(raio, 2);
+
+// Exibição da área da pizza
+Console.WriteLine($"A área da pizza é de {area:F2} cm².");
