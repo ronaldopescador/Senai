43c90d5e1261d6265b97f980ba6ee8707cf6697a
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-10 19:39:56 -0300

    Sequencial19

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial19/Sequencial19/Program.cs
@@ -0,0 +1,15 @@
+﻿// Leitura do número de frangos
+Console.Write("Digite o número de frangos: ");
+int numeroFrangos = int.Parse(Console.ReadLine());
+
+// Cálculo do custo total dos anéis com chip
+double custoAnelIdentificacao = numeroFrangos * 4.0;
+
+// Cálculo do custo total dos anéis de alimento
+double custoAnelAlimento = numeroFrangos * 2 * 3.5;
+
+// Cálculo do custo total
+double custoTotal = custoAnelIdentificacao + custoAnelAlimento;
+
+// Exibição do custo total
+Console.WriteLine($"Custo total: R${custoTotal:F2}.");
