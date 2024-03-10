75a7d6eb8da85f77ed4aefeea5db82ff010e0791
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-09 21:23:48 -0300

    Sequencial08

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial08/Sequencial08/Program.cs
@@ -0,0 +1,17 @@
+﻿// Leitura da primeira nota
+Console.Write("Digite a primeira nota: ");
+double nota1 = Convert.ToDouble(Console.ReadLine());
+
+// Leitura da segunda nota
+Console.Write("Digite a segunda nota: ");
+double nota2 = Convert.ToDouble(Console.ReadLine());
+
+// Leitura da terceira nota
+Console.Write("Digite a terceira nota: ");
+double nota3 = Convert.ToDouble(Console.ReadLine());
+
+// Cálculo da média ponderada
+double mediaPonderada = (nota1 * 1 + nota2 * 2 + nota3 * 3) / 6;
+
+// Exibição da média ponderada
+Console.WriteLine($"A média ponderada do aluno é {mediaPonderada:F2}.");
