4b1ee2319e39d1460a3fe7405bf1b29c47d41ed0
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-10 18:59:08 -0300

    Sequencial13

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial13/Sequencial13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Leitura do número inteiro
+Console.Write("Digite um número inteiro (até três dígitos): ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+// Extração da centena
+int centena = numero / 100;
+
+// Extração da dezena
+int dezena = (numero % 100) / 10;
+
+// Extração da unidade
+int unidade = numero % 10;
+
+// Exibição da centena, dezena e unidade
+Console.WriteLine($"Centena: {centena}");
+Console.WriteLine($"Dezena: {dezena}");
+Console.WriteLine($"Unidade: {unidade}");
