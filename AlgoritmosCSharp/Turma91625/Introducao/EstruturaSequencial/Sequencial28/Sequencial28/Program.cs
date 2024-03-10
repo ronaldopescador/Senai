df667d63f33e8a93e310d44b6b7506d825896f45
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-10 20:30:05 -0300

    Sequencial28

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial28/Sequencial28/Program.cs
@@ -0,0 +1,9 @@
+﻿// Leitura do valor do cheque
+Console.Write("Digite o valor do cheque: ");
+double valorCheque = double.Parse(Console.ReadLine());
+
+// Cálculo do valor do CPMF
+double valorCPMF = valorCheque * 0.0038;
+
+// Exibição do valor do CPMF
+Console.WriteLine($"Valor do CPMF: {valorCPMF:F2}.");
