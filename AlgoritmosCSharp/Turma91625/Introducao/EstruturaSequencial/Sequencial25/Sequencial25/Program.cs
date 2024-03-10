555bc31a40cf6f2c8133bfe8284d04e43ff546cf
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-10 20:16:37 -0300

    Sequencial25

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial25/Sequencial25/Program.cs
@@ -0,0 +1,13 @@
+﻿// Leitura do raio da base da caixa d'água
+Console.Write("Digite o raio da base da caixa d'água (em metros): ");
+double raioBase = double.Parse(Console.ReadLine());
+
+// Leitura da altura da caixa d'água
+Console.Write("Digite a altura da caixa d'água (em metros): ");
+double altura = double.Parse(Console.ReadLine());
+
+// Cálculo do volume da caixa d'água
+double volume = 3.14 * Math.Pow(raioBase, 2) * altura;
+
+// Exibição do volume da caixa d'água
+Console.WriteLine($"Volume da caixa d'água: {volume:F2} metros cúbicos.");
