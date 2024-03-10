7126c2f70e48af22f64fc0f490e51cf3ecb371f7
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-09 21:27:27 -0300

    Sequencial09

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial09/Sequencial09/Program.cs
@@ -0,0 +1,17 @@
+﻿// Leitura da quantidade de camisetas pequenas
+Console.Write("Digite a quantidade de camisetas pequenas: ");
+int quantidadePequena = Convert.ToInt32(Console.ReadLine());
+
+// Leitura da quantidade de camisetas médias
+Console.Write("Digite a quantidade de camisetas médias: ");
+int quantidadeMedia = Convert.ToInt32(Console.ReadLine());
+
+// Leitura da quantidade de camisetas grandes
+Console.Write("Digite a quantidade de camisetas grandes: ");
+int quantidadeGrande = Convert.ToInt32(Console.ReadLine());
+
+// Cálculo do valor total da venda
+double valorTotal = quantidadePequena * 10 + quantidadeMedia * 12 + quantidadeGrande * 15;
+
+// Exibição do valor total da venda
+Console.WriteLine($"O valor total da venda é {valorTotal:F2}.");
