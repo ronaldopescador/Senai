cd59e498ff080f748908ed6e85adc2ed2a794f99
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-10 19:44:49 -0300

    Sequencial20

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial20/Sequencial20/Program.cs
@@ -0,0 +1,13 @@
+﻿// Leitura do número de blusas
+Console.Write("Digite o número de blusas: ");
+int numeroBlusas = int.Parse(Console.ReadLine());
+
+// Leitura da quantidade de novelos de lã utilizados
+Console.Write("Digite a quantidade de novelos de lã utilizados: ");
+int quantidadeNovelos = int.Parse(Console.ReadLine());
+
+// Cálculo da quantidade de novelos por blusa
+double novelosPorBlusa = (double)quantidadeNovelos / numeroBlusas;
+
+// Exibição da quantidade de novelos por blusa
+Console.WriteLine($"Novelos por blusa: {novelosPorBlusa:F2}.");
