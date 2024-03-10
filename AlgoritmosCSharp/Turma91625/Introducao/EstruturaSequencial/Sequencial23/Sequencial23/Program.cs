08ebe7f80d4926b5986e5d4c5ff0a56235240a4c
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-10 20:04:41 -0300

    Sequencial23

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial23/Sequencial23/Program.cs
@@ -0,0 +1,17 @@
+﻿// Leitura da altura da sombra da pessoa
+Console.Write("Digite a altura da sombra da pessoa: ");
+double alturaSombraPessoa = double.Parse(Console.ReadLine());
+
+// Leitura da altura da sombra do prédio
+Console.Write("Digite a altura da sombra do prédio: ");
+double alturaSombraPredio = double.Parse(Console.ReadLine());
+
+// Leitura da altura da pessoa
+Console.Write("Digite altura da pessoa: ");
+double pessoaAltura = double.Parse(Console.ReadLine());
+
+// Cálculo da altura do prédio
+double alturaPredio = (alturaSombraPredio * pessoaAltura) / alturaSombraPessoa;
+
+// Exibição da altura do prédio
+Console.WriteLine($"Altura do prédio: {alturaPredio:F2} metros.");
