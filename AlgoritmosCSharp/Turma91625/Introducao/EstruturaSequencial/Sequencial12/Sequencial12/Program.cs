266e83604bce2c744faa577f17c4b457ba23e708
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-10 18:56:07 -0300

    Sequencial12

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial12/Sequencial12/Program.cs
@@ -0,0 +1,24 @@
+﻿// Leitura do salário do funcionário
+Console.Write("Digite o salário do funcionário: ");
+double salarioInicial = Convert.ToDouble(Console.ReadLine());
+
+// Cálculo do valor do aumento
+double aumento = salarioInicial * 15 / 100;
+
+// Cálculo do salário com o aumento, reaujustado
+double salarioReajustado = salarioInicial + aumento;
+
+// Cálculo do valor do imposto
+double imposto = salarioReajustado * 8 / 100;
+
+// Cálculo do salário final
+double salarioFinal = salarioReajustado - imposto;
+
+// Exibição do salário inicial
+Console.WriteLine($"Salário inicial: {salarioInicial:F2}.");
+
+// Exibição do salário com aumento
+Console.WriteLine($"Salário com aumento: {salarioReajustado:F2}.");
+
+// Exibição do salário final
+Console.WriteLine($"Salário final: {salarioFinal:F2}.");
