ea7ed78034fbefcb296dc1e305c2c07277d265ab
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-10 20:41:35 -0300

    Sequencial29

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial29/Sequencial29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Declarando as variáveis
+double a, b, x;
+
+// Leitura dos coeficientes a e b
+Console.WriteLine("Digite o valor de a: ");
+a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor de b: ");
+b = double.Parse(Console.ReadLine());
+
+// Cálculo da coordenada x
+x = -b / a;
+
+// Exibição das coordenadas
+Console.WriteLine("O ponto toca o eixo x na coordenada (" + x + ", 0).");
