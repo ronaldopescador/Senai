ce23260c7101a6532c112ebba8809db6b3310e68
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-03-10 19:33:44 -0300

    Sequencial17

---
+++ AlgoritmosCSharp/Turma91625/Introducao/EstruturaSequencial/Sequencial17/Sequencial17/Program.cs
@@ -0,0 +1,9 @@
+﻿// Leitura da temperatura em Celsius
+Console.Write("Digite a temperatura em Celsius: ");
+double temperaturaCelsius = double.Parse(Console.ReadLine());
+
+// Conversão de Celsius para Fahrenheit
+double temperaturaFahrenheit = (temperaturaCelsius * 9 / 5) + 32;
+
+// Exibição da temperatura em Fahrenheit
+Console.WriteLine($"Temperatura em Fahrenheit: {temperaturaFahrenheit:F2}°F.");
